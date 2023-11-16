836b6852eb409c8b0d69da554cec12c26ec26c87
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-11-16 13:32:56 +0200

    feat(#5): add React imports in AboutVilnaSection.tsx and AboutVilnaMainContent.tsx

--- src/features/landing/components/AboutVilnaSection/AboutVilnaSection/AboutVilnaSection.tsx
+++ src/features/landing/components/AboutVilnaSection/AboutVilnaSection/AboutVilnaSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import AboutVilnaMainShape from '@/features/landing/assets/svg/AboutVilnaMainShape.svg';
 import {
   scrollToRegistrationSection,
