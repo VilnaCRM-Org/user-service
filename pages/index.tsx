3a113dde86b1ee84288123e15b15f1f34eb1c619
vilnacrm <114362021+vilnacrm@users.noreply.github.com>
2023-11-19 18:35:27 +0100

    feat(#8): add deploy for prod
    * feat(#1): add deploying for sandboxes
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#1): fix deploy-sandbox.yml
    
    * feat(#8): fix deploy to prod
    
    * feat(#8): fix memory leak test
    
    ---------
    
    Co-authored-by: Yaroslav Kravtsov <yaroslav.kravtsov@debricked.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,10 @@ import { useTranslation } from 'react-i18next';
 export default function Home() {
   const { t } = useTranslation();
 
-  const click = () => {
-    setTimeout(() => {
-      console.log('done');
-    }, 2000);
-  };
-
   return (
-    <div>
+    <h1>
       {/* eslint-disable-next-line react/button-has-type */}
-      <button onClick={click}>{ t('hello') }</button>
-    </div>
+      { t('coming-soon') }
+    </h1>
   );
 }
