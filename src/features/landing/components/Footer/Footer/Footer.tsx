de7f27052c827566ed1327a2c67b4f1337b27cc6
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-11-20 22:50:18 +0200

    feat(#5): footer for desktop

---
+++ src/features/landing/components/Footer/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+import FooterBottom from '@/features/landing/components/Footer/FooterBottom/FooterBottom';
+import FooterCopyright from '@/features/landing/components/Footer/FooterCopyright/FooterCopyright';
+import FooterEmail from '@/features/landing/components/Footer/FooterEmail/FooterEmail';
+import FooterHead from '@/features/landing/components/Footer/FooterHead/FooterHead';
+import FooterSocials from '@/features/landing/components/Footer/FooterSocials/FooterSocials';
+import { useScreenSize } from '@/features/landing/hooks/useScreenSize/useScreenSize';
+
+export default function Footer() {
+  const { isSmallest, isMobile, isTablet } = useScreenSize();
+
+  return (
+    <footer style={{
+      paddingLeft: (isSmallest || isMobile || isTablet) ? '5px' : '0',
+      paddingRight: (isSmallest || isMobile || isTablet) ? '5px' : '0',
+    }}>
+      <FooterHead />
+      <FooterBottom>
+        <FooterCopyright />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+          <FooterEmail />
+          <FooterSocials />
+        </Box>
+      </FooterBottom>
+    </footer>
+  );
+}
