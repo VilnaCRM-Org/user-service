1df22fec515e5e770b12937438f8605b052f174f
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2024-01-08 15:35:33 +0200

    fix(#1): fix wrong import

--- src/Shared/Infrastructure/RetryStrategy/InfiniteRetryStrategy.php
+++ src/Shared/Infrastructure/RetryStrategy/InfiniteRetryStrategy.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace App\Shared\Infrastructure\RetryStrategy;
 
 use Symfony\Component\HttpClient\Response\AsyncContext;
-use Symfony\Component\HttpClient\Retry\RetryStrategyInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Retry\RetryStrategyInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class InfiniteRetryStrategy implements RetryStrategyInterface
@@ -16,6 +17,16 @@ class InfiniteRetryStrategy implements RetryStrategyInterface
     }
 
     public function getDelay(AsyncContext $context, ?string $responseContent, ?TransportExceptionInterface $exception): int
+    {
+        return 60000;
+    }
+
+    public function isRetryable(Envelope $message, \Throwable $throwable = null): bool
+    {
+        return true;
+    }
+
+    public function getWaitingTime(Envelope $message, \Throwable $throwable = null): int
     {
         return 60000;
     }
