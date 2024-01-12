e6a7849cedc26886c9bdad4c8506e6b1a2088e29
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2024-01-12 11:49:57 +0200

    fix(#1): fix CORS issue for dev env

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
