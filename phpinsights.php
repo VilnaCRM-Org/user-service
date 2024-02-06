33ac46188a08cbd905ee973094c23f60206e182d
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2024-02-06 17:54:22 +0200

    fix(#1): fix restler check

--- phpinsights.php
+++ phpinsights.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenNormalClasses;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting\SpaceAfterNotSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Strings\UnnecessaryStringConcatSniff;
 use SlevomatCodingStandard\Sniffs\Classes\SuperfluousExceptionNamingSniff;
 use SlevomatCodingStandard\Sniffs\Classes\SuperfluousInterfaceNamingSniff;
 use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
@@ -25,19 +27,32 @@ return [
         UseSpacingSniff::class,
     ],
     'config' => [
+        LineLengthSniff::class => [
+            'exclude' => [
+                'phpinsights',
+            ],
+        ],
         ForbiddenNormalClasses::class => [
             'exclude' => [
                 'src/Shared/Infrastructure/Bus/Command/InMemorySymfonyCommandBus',
                 'src/Shared/Infrastructure/Bus/Event/InMemorySymfonyEventBus',
+                'src/Shared/OpenApi/Factory/Response/DuplicateEmailResponseFactory',
                 'src/User/Domain/Entity/User',
+            ],
+        ],
+        UnnecessaryStringConcatSniff::class => [
+            'exclude' => [
+                'src/Shared/OpenApi/Factory/Response/UnsupportedGrantTypeResponseFactory',
+                'src/User/Infrastructure/Exception/DuplicateEmailException',
+                'src/Shared/OpenApi/Factory/Response/DuplicateEmailResponseFactory',
             ],
         ],
     ],
     'requirements' => [
-        'min-quality' => 95,
+        'min-quality' => 100,
         'min-complexity' => 95,
-        'min-architecture' => 95,
-        'min-style' => 95,
+        'min-architecture' => 100,
+        'min-style' => 100,
     ],
     'threads' => null,
 ];
