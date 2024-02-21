294c6fa11517f6c5212ffe6b4b12060d55477447
Vlas Dvorovyi <vlas20421@gmail.com>
2024-02-21 15:33:00 +0200

    fix(#5): update memoryLeak

--- memlab/memoryLeakTest.js
+++ memlab/memoryLeakTest.js
@@ -1,7 +1,14 @@
+// Todo: remove eslint rules
+/* eslint-disable no-empty-function */
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 
 function url() {
   return process.env.WEBSITE_URL;
 }
 
-module.exports = { url };
+async function action() {}
+
+async function back() {}
+
+module.exports = { url, action, back };
