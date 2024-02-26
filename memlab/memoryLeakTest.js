71ae934952951c734015088096a77d19a5c1e26a
Vlas Dvorovyi <vlas20421@gmail.com>
2024-02-26 18:27:02 +0200

    fix(#5): fix memory-leak

--- memlab/memoryLeakTest.js
+++ memlab/memoryLeakTest.js
@@ -4,12 +4,4 @@ function url() {
   return process.env.WEBSITE_URL;
 }
 
-async function action() {
-
-}
-
-async function back() {
-
-}
-
-module.exports = { action, back, url };
+module.exports = { url };
