77cfd5adcbc7b03e3eff272d38308459f047bc50
Vlas Dvorovyi <vlas20421@gmail.com>
2024-02-27 16:57:22 +0200

    fix(#3): fix Ci checks

--- memlab/memoryLeakTest.js
+++ memlab/memoryLeakTest.js
@@ -3,12 +3,8 @@ require('dotenv').config();
 function url() {
   return process.env.WEBSITE_URL;
 }
-async function action() {
+async function action() {}
 
-}
-
-async function back() {
-
-}
+async function back() {}
 
 module.exports = { action, back, url };
