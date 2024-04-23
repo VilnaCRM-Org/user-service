6a4349785b899b9a9b9adb0d09063aea8e79c111
Igor Pereimyvovk <igor.pereimyvovk@gmail.com>
2024-04-23 19:06:59 +0300

    fix(#5): project build

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const LocalizationGenerator = require('./scripts/localizationGenerator');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/robots.txt',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'text/plain',
-          },
-        ],
-      },
-    ];
-  },
   output: 'export',
   images: {
     unoptimized: true,
