2c6a171a79356902c2e561a8d590c2fff5856122
Igor Pereimyvovk <igor.pereimyvovk@gmail.com>
2024-04-30 09:18:32 +0300

    fix(#5): refactor initials with related tests

--- src/features/landing/components/AuthSection/Validations/initials.ts
+++ src/features/landing/components/AuthSection/Validations/initials.ts
@@ -2,13 +2,16 @@ import { t } from 'i18next';
 
 const MAX_INITIALS_LENGTH: number = 255;
 
+export const isValidFullName: (fullName: string) => boolean = (
+  fullName: string
+): boolean => Boolean(fullName) && fullName.length <= MAX_INITIALS_LENGTH;
+
 const validateFullName: (fullName: string) => string | boolean = (
   fullName: string
 ): string | boolean => {
   const trimmedFullName: string = fullName.trim();
 
-  if (Boolean(trimmedFullName) && trimmedFullName.length <= MAX_INITIALS_LENGTH)
-    return true;
+  if (isValidFullName(trimmedFullName)) return true;
 
   return t('sign_up.form.name_input.error_text');
 };
--- src/test/unit/initials-validation.test.ts
+++ src/test/unit/initials-validation.test.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 import { validateFullName } from '../../features/landing/components/AuthSection/Validations';
+import { isValidFullName } from '../../features/landing/components/AuthSection/Validations/initials';
 
 const testFullName: string = faker.person.fullName();
 const testFirstName: string = faker.person.firstName();
@@ -8,6 +9,22 @@ const testSecondName: string = faker.person.lastName();
 const errorText: string = 'Invalid full name format';
 
 describe('initials Tests', () => {
+  describe('isValidFullName', () => {
+    it('should return true if the full name is valid', () => {
+      expect(isValidFullName(testFullName)).toBe(true);
+      expect(isValidFullName(`!@${testFullName} #%`)).toBe(true);
+      expect(isValidFullName(`123 ${testFullName}456`)).toBe(true);
+      expect(isValidFullName(`${testFirstName} ${testSecondName}`)).toBe(true);
+      expect(isValidFullName(`123 456 789`)).toBe(true);
+      expect(isValidFullName(`!@# $%^ &*()`)).toBe(true);
+    });
+
+    it('should return false if the full name is not valid', () => {
+      expect(validateFullName(``)).toBe(errorText);
+      expect(validateFullName(testFullName.repeat(100))).toBe(errorText);
+    });
+  });
+
   describe('validateFullName', () => {
     it('should return true if the full name is valid', () => {
       expect(validateFullName(testFullName)).toBe(true);
@@ -24,7 +41,7 @@ describe('initials Tests', () => {
       expect(validateFullName(`  `)).toBe(errorText);
       expect(validateFullName(`\n`)).toBe(errorText);
       expect(validateFullName(` \n `)).toBe(errorText);
-      expect(validateFullName(testFullName.repeat(40))).toBe(errorText);
+      expect(validateFullName(testFullName.repeat(100))).toBe(errorText);
     });
   });
 });
