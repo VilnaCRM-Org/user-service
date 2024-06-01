97a0f426e75a8de80ff73178f3e7661916cc1604
Vlas Dvorovyi <vlas20421@gmail.com>
2024-06-01 16:57:10 +0300

    fix(5): returned the old code

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ const LocalizationGenerator = require('./scripts/localizationGenerator');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Is necessary to prevent an error from Next.js. https://nextjs.org/docs/messages/export-image-api
+  },
   reactStrictMode: true,
   swcMinify: true,
+
   webpack: config => {
     const localizationGenerator = new LocalizationGenerator();
     localizationGenerator.generateLocalizationFile();
