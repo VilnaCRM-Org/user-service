a3d346586876a5b25492fde0b92918abc29abfe5
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-06-12 00:35:44 +0300

    test(#5): increase time between screenshots

--- src/test/visual/visualComparison.spec.ts
+++ src/test/visual/visualComparison.spec.ts
@@ -6,6 +6,7 @@ test('Visual Test', async ({ page }) => {
   await page.goto('/');
   for (const screen of screenSizes) {
     await page.setViewportSize({ width: screen.width, height: screen.height });
+    await page.waitForTimeout(3000);
     await expect(page).toHaveScreenshot(`${screen.name}.png`, {
       fullPage: true,
     });
