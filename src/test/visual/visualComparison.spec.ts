7e8319ca399c609e678f8bd9d1aa2c803d07e2b6
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-06-12 19:53:49 +0300

    test(#5): visual tests

--- src/test/visual/visualComparison.spec.ts
+++ src/test/visual/visualComparison.spec.ts
@@ -2,13 +2,27 @@ import { test, expect } from '@playwright/test';
 
 import { screenSizes } from './constants';
 
-test('Visual Test', async ({ page }) => {
-  await page.goto('/');
+test.describe('Visual Tests', () => {
   for (const screen of screenSizes) {
-    await page.setViewportSize({ width: screen.width, height: screen.height });
-    await page.waitForTimeout(3000);
-    await expect(page).toHaveScreenshot(`${screen.name}.png`, {
-      fullPage: true,
+    test(`${screen.name} test`, async ({ page }) => {
+      await page.goto('/');
+      await page.waitForTimeout(3000);
+
+      await page.waitForLoadState('networkidle');
+      await page.evaluateHandle('document.fonts.ready');
+
+      const scrollHeight: number = await page.evaluate(() => document.documentElement.scrollHeight);
+      await page.setViewportSize({ width: screen.width, height: scrollHeight });
+
+      await page.waitForTimeout(3000);
+
+      await page.setViewportSize({ width: screen.width, height: screen.height });
+
+      await page.waitForTimeout(3000);
+
+      await expect(page).toHaveScreenshot(`${screen.name}.png`, {
+        fullPage: true,
+      });
     });
   }
 });
