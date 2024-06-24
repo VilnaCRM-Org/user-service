1d6a6979bb095f3a1563cfdab6f1246d5ffb471d
YapieRT <sokolov.valentyn.work@gmail.com>
2024-06-24 12:26:16 +0300

    fix(#5): update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     trace: 'on-first-retry',
     ignoreHTTPSErrors: true,
