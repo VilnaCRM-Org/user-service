fccb1354cfe62902aabccdf129e866515e8911de
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-07-11 11:18:04 +0300

    fix(#24): try to fix OpenApi generation

--- src/Shared/Application/OpenApi/Factory/Request/EmptyRequestFactory.php
+++ src/Shared/Application/OpenApi/Factory/Request/EmptyRequestFactory.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\Request;
 
-use ApiPlatform\OpenApi\Model\MediaType;
 use ApiPlatform\OpenApi\Model\RequestBody;
-use ApiPlatform\OpenApi\Model\Schema;
 use App\Shared\Application\OpenApi\Builder\RequestBuilder;
 
 final class EmptyRequestFactory implements AbstractRequestFactory
@@ -17,18 +15,6 @@ final class EmptyRequestFactory implements AbstractRequestFactory
 
     public function getRequest(): RequestBody
     {
-        $schema = new Schema();
-        $schema['type'] = 'object';
-        $schema['properties'] = new \ArrayObject();
-        $this->requestBuilder->build([]);
-        return new RequestBody(
-            description: 'This operation does not expect a body.',
-            content: new \ArrayObject([
-                'application/json' => new MediaType(
-                    schema: $schema,
-                ),
-            ]),
-            required: false,
-        );
+        return $this->requestBuilder->build([]);
     }
 }
