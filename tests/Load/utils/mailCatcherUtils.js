d4d67e1227173a9c17ec7a14bcfce47d369e4afa
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-11-15 19:37:36 +0200

    fix(#70): add logging for CI

--- tests/Load/utils/mailCatcherUtils.js
+++ tests/Load/utils/mailCatcherUtils.js
@@ -15,7 +15,8 @@ export default class MailCatcherUtils {
 
   async getConfirmationToken(messageId) {
     const message = await http.get(`${this.mailCatcherUrl}/${messageId}.source`);
-
+    console.log("MY MESSAGE")
+    console.log(JSON.stringify(message));
     return this.extractConfirmationToken(message.body);
   }
 
