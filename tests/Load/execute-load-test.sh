8fd52ef4eb7456115e8edee3a820bd746b935eed
Kravalg <github-action@actions-template-sync.noreply.github.com>
2024-12-30 09:09:54 +0000

    chore(template): merge template changes :up:
    Signed-off-by: Kravalg <github-action@actions-template-sync.noreply.github.com>

--- tests/Load/execute-load-test.sh
+++ tests/Load/execute-load-test.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-set -e
+set -e -x
 
 if [ -z "$1" ]; then
     echo "Error: scenario not provided."
-    exit 1
-fi
-
-if [ -z "$2" ]; then
-    echo "Error: runSmoke not provided."
-    exit 1
-fi
-
-if [ -z "$3" ]; then
-    echo "Error: runAverage not provided."
-    exit 1
-fi
-
-if [ -z "$4" ]; then
-    echo "Error: runStress not provided."
-    exit 1
-fi
-
-if [ -z "$5" ]; then
-    echo "Error: runSpike not provided."
     exit 1
 fi
 
@@ -33,13 +13,11 @@ runStress=$4
 runSpike=$5
 htmlPrefix=$6
 
+echo "Executing load test for scenario: $scenario"
+echo "Options - Smoke: $runSmoke, Average: $runAverage, Stress: $runStress, Spike: $runSpike"
 K6="docker run -v ./tests/Load:/loadTests --net=host --rm \
     --user $(id -u) \
     k6 run --summary-trend-stats='avg,min,med,max,p(95),p(99)' \
-    --out 'web-dashboard=period=1s&export=/loadTests/loadTestsResults/${htmlPrefix}${scenario}.html'"
-
-if [[ $scenario != "createUser" && $scenario != "graphQLCreateUser" && $scenario != "createUserBatch" ]]; then
-  eval "$K6" /loadTests/utils/prepareUsers.js -e scenarioName="${scenario}" -e run_smoke="${runSmoke}" -e run_average="${runAverage}" -e run_stress="${runStress}" -e run_spike="${runSpike}"
-fi
+    --out 'web-dashboard=period=1s&export=/loadTests/results/${htmlPrefix}${scenario}.html'"
 
 eval "$K6" "/loadTests/scripts/${scenario}.js" -e run_smoke="${runSmoke}" -e run_average="${runAverage}" -e run_stress="${runStress}" -e run_spike="${runSpike}"
