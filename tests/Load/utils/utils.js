43735a188007723b3451073f0e64b3d6050a9092
Vadim <kostiuk.v@unweb.com.ua>
2025-06-07 15:52:29 +0300

    test(#188): fix error

--- tests/Load/utils/utils.js
+++ tests/Load/utils/utils.js
@@ -16,10 +16,10 @@ export default class Utils {
   getConfig() {
     try {
       return JSON.parse(open('../config.json'));
-    } catch {
+    } catch (_) {
       try {
         return JSON.parse(open('../config.json.dist'));
-      } catch {
+      } catch (_) {
         throw new Error('Error occurred while trying to open config');
       }
     }
