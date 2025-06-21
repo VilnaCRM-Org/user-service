c5467451be6fccc66a42fb43229c7bfde6a75b26
KostiukVM <152398688+KostiukVM@users.noreply.github.com>
2025-06-21 08:44:41 +0000

    chore(#193): fix linting issues
    Signed-off-by: Vadym <kostiukdsfv@gmail.com>

--- tests/Load/scripts/resendEmailToUser.js
+++ tests/Load/scripts/resendEmailToUser.js
@@ -31,7 +31,7 @@ export default function resendEmail(data) {
   const response = http.post(
     `${utils.getBaseHttpUrl()}/${id}/resend-confirmation-email`,
     JSON.stringify(null),
-    utils.getJsonHeader(),
+    utils.getJsonHeader()
   );
 
   utils.checkResponse(response, 'is status 200', res => res.status === 200);
