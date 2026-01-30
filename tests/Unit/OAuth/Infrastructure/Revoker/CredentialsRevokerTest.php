1b042a0fd39b0fd5bcd27d2a5d7a2c1761e2a3ff
valerii <vsierhieiev-its24@lll.kpi.ua>
2026-01-31 01:31:13 +0200

    refactor: rename CredentialsRevokerTest namespace for better organization

--- tests/Unit/OAuth/Infrastructure/Revoker/CredentialsRevokerTest.php
+++ tests/Unit/OAuth/Infrastructure/Revoker/CredentialsRevokerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Unit\OAuth\Infrastructure\Service;
+namespace App\Tests\Unit\OAuth\Infrastructure\Revoker;
 
 use App\OAuth\Infrastructure\Revoker\CredentialsRevoker;
 use App\Tests\Unit\OAuth\Infrastructure\OAuthInfrastructureTestCase;
